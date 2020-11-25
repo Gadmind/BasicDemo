b73da90cbe0950d274833e6977fc8a2a554d84c4
Gadmind <daopudu@sina.com>
2020-11-25 17:08:03 +0800

    测试接口

---
+++ src/main/java/com/daop/basic/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.daop.basic.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: demo
+ * @BelongsPackage: com.daop.basic.demo.controller
+ * @Description: 测试接口
+ * @DATE: 2020-11-25
+ * @AUTHOR: Administrator
+ **/
+@RestController
+public class TestController {
+
+}
